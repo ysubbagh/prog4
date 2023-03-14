78fcf72d085801d4dd1bc4deaa857dcfdbb2aa9d
ysubbagh <48531617+ysubbagh@users.noreply.github.com>
2023-03-14 23:15:49 +0000

    build driver

--- biz.cpp
+++ biz.cpp
@@ -1,2 +1,29 @@
 #include "biz.h"
 
+//constructor
+Biz:: Biz(){
+
+
+}
+
+//functions//
+//load in new movie
+bool Biz:: buildMovie(string info){
+
+
+    return true; // basecase
+}
+
+//build a customer profile
+bool Biz:: buildCust(string info){
+
+
+    return true; // basecase
+}
+
+//process a transaction
+bool Biz:: transaction(string info){
+
+
+    return true; // basecase
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //include header files
 #include <iostream>
 #include <vector> 
+#include <fstream>
 #include "biz.h"
 
 using namespace std;
@@ -20,5 +21,52 @@ int main() {
    print out movie infos
 
 */
+   //create the shop
+   Biz shop;
+
+   //load in the movies into the movie shop
+   string line;
+   ifstream infile1("data4movies.txt");
+   if (!infile1) {
+      cout << "File could not be opened." << endl;
+      return 1;
+   }
+   //do stuff for movies
+   for (;;) {
+      if (infile1.eof()) break;
+      //stuff
+      getline(infile1, line);
+      shop.buildMovie(line);
+   }
+
+   // load in customers into the moveie shop
+   ifstream infile2("data4customers.txt");
+   if (!infile2) {
+      cout << "File could not be opened." << endl;
+      return 1;
+   }
+   //do stuff for customer
+   for (;;) {
+      if (infile2.eof()) break;
+      //stuff
+      getline(infile2, line);
+      shop.buildCust(line);
+   }
+
+   //load in the commands for the transaction history
+   ifstream infile3("data4commands.txt");
+   if (!infile3) {
+      cout << "File could not be opened." << endl;
+      return 1;
+   }
+   //do transaction stuff
+   for (;;) {
+      if (infile3.eof()) break;
+      //stuff
+      getline(infile3, line);
+      shop.transaction(line);
+   }
+
+   //print out output
 
 }
